2d34aad01cf4fa204d67b962f28d082c1e1618e7
GregoryMaselle-W <Gregory.Maselle@bbd.co.za>
2025-07-02 22:22:16 +0200

    Using correct routes for transaction

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import companyRoutes from "./routes/companyRoutes";
 import TransactionsController from "./controllers/transactionsController";
+import transactionRoutes from "./routes/transactionRoutes";
 import pickupRequestRoutes from "./routes/pickupRequestRoutes"
 import AppError from "./utils/appError";
 import globalErrorHandler from "./controllers/errorController";
@@ -10,14 +11,14 @@ const PORT = 3000;
 
 app.use(express.json());
 
-// app.use("/api/transactions", TransactionsController.routes());
+app.use("/api/transactions", transactionRoutes);
 app.use("/api/company", companyRoutes);
 app.use("/api/pickup-request", pickupRequestRoutes)
 app.all('*', (req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use(globalErrorHandler);
+app.use(globalErrorHandler); // error handler middleware.
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
