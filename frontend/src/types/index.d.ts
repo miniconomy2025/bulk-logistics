2c4be6a378ddf1619966b9dcd518e5a542311550
KeloDraken <65687571+KeloDraken@users.noreply.github.com>
2025-07-02 08:43:04 +0000

    Apply frontend formatting changes

--- frontend/src/types/index.d.ts
+++ frontend/src/types/index.d.ts
@@ -1,6 +1,6 @@
 export interface NavItem {
-  title: string;
-  href: string;
-  icon: React.JSX.IntrinsicElements.span;
-  isActive?: boolean;
+    title: string;
+    href: string;
+    icon: React.JSX.IntrinsicElements.span;
+    isActive?: boolean;
 }
