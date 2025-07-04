659b6860d55eb32e180de91139408ed9ef21b0a8
Lerato Taunyane <Lerato.Taunyane@bbd.co.za>
2025-07-04 19:35:42 +0200

    PR fix

--- backend/src/repositories/companyRepository.ts
+++ backend/src/repositories/companyRepository.ts
@@ -2,14 +2,12 @@ import database from "../config/database";
 
 export const insertCompany = async (companyName: string, certificate_identifier: string, bank_account_number: string) => {
     const query = "INSERT INTO company (company_name, certificate_identifier, bank_account_number) VALUES ($1, $2, $3) RETURNING *";
-    const values = [companyName, certificate_identifier, bank_account_number];
-    const result = await database.query(query, values);
+    const result = await database.query(query, [companyName, certificate_identifier, bank_account_number]);
     return result.rows[0];
 };
 
 export const getCertificateByCompanyName = async (companyName: string) => {
     const query = "SELECT certificate_identifier FROM company WHERE company_name = $1";
-    const values = [companyName];
-    const result = await database.query(query, values);
+    const result = await database.query(query, [companyName]);
     return result.rows[0]?.certificate_identifier || null;
 };
