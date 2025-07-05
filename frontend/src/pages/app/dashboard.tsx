56c001b0f31a0e781b6c4aa21b943ffe6efaebee
Jag kan HTML <vmama@ombank.co.za>
2025-07-05 17:33:09 +0200

    feat: replace SVG icons with Material Symbols in dashboard metrics

--- frontend/src/pages/app/dashboard.tsx
+++ frontend/src/pages/app/dashboard.tsx
@@ -132,22 +132,7 @@ const Dashboard: React.FC = () => {
                         changeType="increase"
                         bgColor="bg-green-100"
                         textColor="text-green-600"
-                        icon={
-                            <svg
-                                className="h-6 w-6"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.592 1L21 12m-6 4h2.586a1 1 0 01.707.293l2.414 2.414a1 1 0 01-.707 1.707H3.707a1 1 0 01-.707-1.707l2.414-2.414A1 1 0 017.414 16H10m-2.592-8a2 2 0 012.592-1z"
-                                ></path>
-                            </svg>
-                        }
+                        icon={<span className="material-symbols-outlined">attach_money</span>}
                     />
                     <MetricCard
                         title="Total Expenses"
@@ -156,22 +141,7 @@ const Dashboard: React.FC = () => {
                         changeType="decrease"
                         bgColor="bg-red-100"
                         textColor="text-red-600"
-                        icon={
-                            <svg
-                                className="h-6 w-6"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"
-                                ></path>
-                            </svg>
-                        }
+                        icon={<span className="material-symbols-outlined">account_balance_wallet</span>}
                     />
                     <MetricCard
                         title="Net Profit"
@@ -180,22 +150,7 @@ const Dashboard: React.FC = () => {
                         changeType="increase"
                         bgColor="bg-blue-100"
                         textColor="text-blue-600"
-                        icon={
-                            <svg
-                                className="h-6 w-6"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0h.01M9 19H7a2 2 0 01-2-2v-6a2 2 0 012-2h2m2-7v11m3-4h.01M17 19h2a2 2 0 002-2v-6a2 2 0 00-2-2h-2a2 2 0 00-2 2v6a2 2 0 002 2z"
-                                ></path>
-                            </svg>
-                        }
+                        icon={<span className="material-symbols-outlined">money_bag</span>}
                     />
                     <MetricCard
                         title="Active Shipments"
@@ -204,22 +159,7 @@ const Dashboard: React.FC = () => {
                         changeType="increase"
                         bgColor="bg-orange-100"
                         textColor="text-orange-600"
-                        icon={
-                            <svg
-                                className="h-6 w-6"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
-                                ></path>
-                            </svg>
-                        }
+                        icon={<span className="material-symbols-outlined">assignment</span>}
                     />
                 </section>
 
