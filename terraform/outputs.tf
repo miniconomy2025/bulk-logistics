5839df2c475d1fb9a475710c7b858ea22deec24a
Lerato Taunyane <Lerato.Taunyane@bbd.co.za>
2025-07-08 07:28:18 +0200

    To be removed later

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,3 +8,7 @@ output "frontend_public_ip" {
 output "key_pair_name" {
   value = aws_key_pair.generated_key.key_name
 }
+output "private_key_pem" {
+  value     = tls_private_key.ec2_key.private_key_pem
+  sensitive = true
+}
