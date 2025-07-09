abcc57ea7b053b6eb08de1343981d517b2664680
ndzalama-mabasa <195544527+ndzalama-mabasa@users.noreply.github.com>
2025-07-09 00:30:53 +0000

    Apply backend formatting changes

--- backend/src/models/vehicle.ts
+++ backend/src/models/vehicle.ts
@@ -13,7 +13,7 @@ export const addVehicle = async (vehicleInfo: VehicleCreate) => {
     const sql = "CALL insert_vehicle_info(p_name => $1, p_purchase_date => $2, p_daily_operational_cost => $3, p_maximum_capacity => $4)";
 
     const values = [vehicleInfo.type, vehicleInfo.purchase_date, vehicleInfo.operational_cost, vehicleInfo.load_capacity];
-    
+
     const res = await db.query(sql, values);
 
     if (res.rowCount === 0) {
