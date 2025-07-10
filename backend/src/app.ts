019afc510fac3aee82790e329b3f3d1d8cdb7aec
Lerato Taunyane <Lerato.Taunyane@bbd.co.za>
2025-07-10 13:49:15 +0200

    revert

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,6 @@
 import express from "express";
-import fs from "fs";
-import https from "https";
 
 import { init } from "express-oas-generator";
-
-import cors from "cors"
 
 import bankRoutes from "./routes/bankNotification";
 import companyRoutes from "./routes/companyRoutes";
@@ -25,50 +21,35 @@ init(app, {
         version: "1.0.0",
         description: `The Bulk Logistics API enables the transfer of goods and machinery between suppliers and phone manufacturing companies within the simulated cellphone supply chain economy.
 
-Bulk Logistics plays a critical role in ensuring the continuous flow of manufactured components—such as electronics screens, and cases from suppliers to phone companies (e.g Pear and SumSang). It may also handle delivery of equipment sourced from The Hand of Hḗphaistos, and Recycler to companies within the supply chain.
+    Bulk Logistics plays a critical role in ensuring the continuous flow of manufactured components—such as electronics screens, and cases from suppliers to phone companies (e.g Pear and SumSang). It may also handle delivery of equipment sourced from The Hand of Hḗphaistos, and Recycler to companies within the supply chain.
 
-Key responsibilities include:
-- Managing shipment orders between entities
-- Tracking goods in transit
-- Supporting logistics analytics and capacity planning
-- Coordinating with manufacturing schedules and raw material supply
-description: Production server`,
+    Key responsibilities include:
+    - Managing shipment orders between entities
+    - Tracking goods in transit
+    - Supporting logistics analytics and capacity planning
+    - Coordinating with manufacturing schedules and raw material supply
+    description: Production server`,
     },
 });
 
-const PORT = 443; // standard HTTPS port
-
-app.use(
-    cors({
-        credentials: true,
-        origin: "*",
-    }),
-);
+const PORT = 3000;
 
 app.use(express.json());
 
-app.use("", thohRoutes);
 app.use("/api/health", health);
 app.use("/api/bank", bankRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/company", companyRoutes);
 app.use("/api/pickup-request", pickupRequestRoutes);
+app.use("/api/thoh", thohRoutes);
 app.use("/api/shipments", shipmentRoutes);
 
-// app.all("*", (req, res, next) => {
-//     next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-// });
+app.all("*", (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 app.use(globalErrorHandler); // error handler middleware.
 
-const options = {
-  key: fs.readFileSync('/etc/ssl/bulk-logistics/bulk-logistics-server.key'),
-  cert: fs.readFileSync('/etc/ssl/bulk-logistics/bulk-logistics-server.crt'),
-  ca: fs.readFileSync('/etc/ssl/bulk-logistics/root-ca.crt'),
-  requestCert: true,
-  rejectUnauthorized: true,  // Enforce client certificate validation
-};
-
-https.createServer(options, app).listen(PORT, () => {
-  console.log(`Server running with mTLS on https://localhost:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
