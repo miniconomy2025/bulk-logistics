fd531039f0eaf2e9ead018bac9ef3835455dc87b
Lerato Taunyane <Lerato.Taunyane@bbd.co.za>
2025-07-10 13:32:00 +0200

    https and terraform changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import fs from "fs";
+import https from "https";
 
 import { init } from "express-oas-generator";
 
@@ -21,18 +23,18 @@ init(app, {
         version: "1.0.0",
         description: `The Bulk Logistics API enables the transfer of goods and machinery between suppliers and phone manufacturing companies within the simulated cellphone supply chain economy.
 
-    Bulk Logistics plays a critical role in ensuring the continuous flow of manufactured components—such as electronics screens, and cases from suppliers to phone companies (e.g Pear and SumSang). It may also handle delivery of equipment sourced from The Hand of Hḗphaistos, and Recycler to companies within the supply chain.
+Bulk Logistics plays a critical role in ensuring the continuous flow of manufactured components—such as electronics screens, and cases from suppliers to phone companies (e.g Pear and SumSang). It may also handle delivery of equipment sourced from The Hand of Hḗphaistos, and Recycler to companies within the supply chain.
 
-    Key responsibilities include:
-    - Managing shipment orders between entities
-    - Tracking goods in transit
-    - Supporting logistics analytics and capacity planning
-    - Coordinating with manufacturing schedules and raw material supply
-    description: Production server`,
+Key responsibilities include:
+- Managing shipment orders between entities
+- Tracking goods in transit
+- Supporting logistics analytics and capacity planning
+- Coordinating with manufacturing schedules and raw material supply
+description: Production server`,
     },
 });
 
-const PORT = 3000;
+const PORT = 443; // standard HTTPS port
 
 app.use(express.json());
 
@@ -50,6 +52,14 @@ app.all("*", (req, res, next) => {
 
 app.use(globalErrorHandler); // error handler middleware.
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+const options = {
+  key: fs.readFileSync('/etc/ssl/bulk-logistics/bulk-logistics-server.key'),
+  cert: fs.readFileSync('/etc/ssl/bulk-logistics/bulk-logistics-server.cert'),
+  ca: fs.readFileSync('/etc/ssl/bulk-logistics/root-ca.crt'),
+  requestCert: true,
+  rejectUnauthorized: true,  // Enforce client certificate validation
+};
+
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`Server running with mTLS on https://localhost:${PORT}`);
 });
