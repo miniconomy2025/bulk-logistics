651db928318c9f62ce50ae28c5b784833e811297
GregoryMaselle-W <Gregory.Maselle@bbd.co.za>
2025-07-12 23:40:26 +0200

    Update

--- backend/src/routes/thohRoutes.ts
+++ backend/src/routes/thohRoutes.ts
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post("/trucks/failure", rateLimiter(), truckFailure);
 router.post("/trucks/delivery", rateLimiter(), truckDelivery);
 router.post("/simulation", rateLimiter(), startSimulation);
-router.delete("/simulation/stop", rateLimiter(), stopSimulation);
+router.delete("/simulation", rateLimiter(), stopSimulation);
 
 export default router;
