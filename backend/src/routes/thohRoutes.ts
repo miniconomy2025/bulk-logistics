0a3fc46927ac9cc885e81eefb6de95afcba7dd84
GregoryMaselle-W <Gregory.Maselle@bbd.co.za>
2025-07-12 23:39:26 +0200

    Use delete instead of post

--- backend/src/routes/thohRoutes.ts
+++ backend/src/routes/thohRoutes.ts
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post("/trucks/failure", rateLimiter(), truckFailure);
 router.post("/trucks/delivery", rateLimiter(), truckDelivery);
 router.post("/simulation", rateLimiter(), startSimulation);
-router.post("/simulation/stop", rateLimiter(), stopSimulation);
+router.delete("/simulation/stop", rateLimiter(), stopSimulation);
 
 export default router;
