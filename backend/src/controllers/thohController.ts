51b6b032224dd353d3584372ae56e7a68af299d1
Lerato Taunyane <Lerato.Taunyane@bbd.co.za>
2025-07-12 21:14:07 +0200

    stop simulation changes

--- backend/src/controllers/thohController.ts
+++ backend/src/controllers/thohController.ts
@@ -4,6 +4,9 @@ import { beginSimulation, handleTruckDelivery, processTruckFailure } from "../se
 import { TruckDelivery } from "../types";
 import { TruckFailureInfo } from "../types/thoh";
 import { handleTruckFailure } from "../services/thohService";
+import { autonomyService } from "../services/AutonomyService";
+import axios from "axios";
+
 
 export const startSimulation = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const { epochStartTime } = req.body;
@@ -29,4 +32,29 @@ export const truckDelivery = catchAsync(async (req: Request, res: Response, next
     const truckDeliveryInfo: TruckDelivery = req.body;
     await handleTruckDelivery(truckDeliveryInfo);
     res.status(200).send();
+});
+
+export const stopSimulation = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    autonomyService.stop();
+
+    try {
+        await axios.post(
+            'https://api.github.com/repos/miniconomy2025/bulk-logistics/actions/workflows/database-migrations.yaml/dispatches',
+            {
+                ref: "main"
+            },
+            {
+                headers: {
+                    Accept: "application/vnd.github+json",
+                    Authorization: `Bearer ${process.env.GITHUB_PAT}`,
+                    "X-GitHub-Api-Version": "2022-11-28",
+                },
+            }
+        );
+
+        res.status(200).json({ message: "Simulation stopped and migration job triggered." });
+    } catch (err: any) {
+        console.error("Failed to trigger GitHub workflow", err?.response?.data || err);
+        res.status(500).json({ error: "Simulation stopped but failed to trigger migration job." });
+    }
 });
