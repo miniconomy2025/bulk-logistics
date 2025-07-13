c3909ac28ee217c146327b6164a5f1045e539f57
ndzalama.mabasa <ndzalama.mabasa@bbd.co.za>
2025-07-14 00:00:50 +0200

    log item unit

--- backend/src/controllers/pickupRequestController.ts
+++ backend/src/controllers/pickupRequestController.ts
@@ -26,11 +26,12 @@ export const createPickupRequest = catchAsync(async (req: Request, res: Response
     }
     let newItems: ItemRequest[] = [];
     const itemDefinition: ItemDefinitionWithName[] = await getItemDefinitions();
-    console.log("~~~~~~~~~~~~~~~~~~~~~~~ Pickup Request Received ~~~~~~~~~~~~~~~~~~~~~~~");
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~ Pickup Request ~~~~~~~~~~~~~~~~~~~~~~~");
     console.log(JSON.stringify(pickupRequestDetails.items, null, 2));
-
+    
     pickupRequestDetails.items.forEach((item) => {
         const itemMeasurementType: "KG" | "UNIT" = itemDefinition.find((i) => i.item_name == item.itemName)!.capacity_type_name as "KG" | "UNIT";
+        console.log(`~~~~~~~~~~~~~~~~~~~~~~~ ${item.itemName} - ${item.quantity} - ${itemMeasurementType} ~~~~~~~~~~~~~~~~~~~~~~~`);
         const itemMaxCapacity = itemMeasurementType === "KG" ? 5000 : 2000;
         if (item.quantity > itemMaxCapacity) {
             const fullTrucks = Math.floor(item.quantity / itemMaxCapacity);
