48876a977014a1a75d7c3e7f76fa67e6091276c8
Jag kan HTML <vmama@ombank.co.za>
2025-07-14 00:26:33 +0200

    Use Link component

--- frontend/src/components/ui/nav-link.tsx
+++ frontend/src/components/ui/nav-link.tsx
@@ -1,13 +1,14 @@
+import { Link } from "react-router";
 import type { NavItem } from "../../types";
 
 export const NavLink: React.FC<NavItem> = ({ title, icon, href }) => {
     return (
-        <a
-            href={href}
+        <Link
+            to={href}
             className="flex items-center rounded-lg p-3 text-gray-700 hover:bg-gray-50"
         >
             <span className="mr-3">{icon}</span>
             {title}
-        </a>
+        </Link>
     );
 };
