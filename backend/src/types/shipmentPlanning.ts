d151e3b36dce2ab2ab8a1addd376bf4efc75db48
GregoryMaselle-W <Gregory.Maselle@bbd.co.za>
2025-07-14 14:43:18 +0200

    Shipment fixed

--- backend/src/types/shipmentPlanning.ts
+++ backend/src/types/shipmentPlanning.ts
@@ -3,14 +3,17 @@ import { PickupToShipmentItemDetails, VehicleWithType } from "./db";
 export interface PlannableVehicle extends VehicleWithType {
     capacityRemaining: number;
     pickupsAssignedToday: number;
+    dropoffsAssignedToday: number; 
     capacity_type_id: number;
+    assignedOrigins: Set<string>;
+    assignedDestinations: Set<string>;
 }
 
 export interface ShipmentPlan {
     vehicle: PlannableVehicle;
     itemsToAssign: PickupToShipmentItemDetails[];
-    originCompanyName: string;
-    destinationCompanyName: string;
+    originCompanyNames: Set<string>;
+    destinationCompanyNames: Set<string>;
 }
 
 export interface DailyPlanOutput {
