c8b288bef26198745ba21375bdc16348041c491a
Lerato Taunyane <Lerato.Taunyane@bbd.co.za>
2025-10-22 08:45:47 +0200

    migration, backend and frontend job tests

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,6 +2,10 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   roots: ['<rootDir>/src'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'jest-transform-stub'
+  },
   testMatch: [
     '**/__tests__/**/*.ts',
     '**/__tests__/**/*.tsx',
