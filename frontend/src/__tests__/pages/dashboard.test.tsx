8744011f6c76ee45f005a79c462bbffb9e64d491
ndzalama.mabasa <ndzalama.mabasa@bbd.co.za>
2025-10-24 09:17:17 +0200

    remove unused packages

--- frontend/src/__tests__/pages/dashboard.test.tsx
+++ frontend/src/__tests__/pages/dashboard.test.tsx
@@ -1,5 +1,4 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render, screen, waitFor } from "@testing-library/react";
 
 import Dashboard from "../../pages/dashboard";
 import "@testing-library/jest-dom";
